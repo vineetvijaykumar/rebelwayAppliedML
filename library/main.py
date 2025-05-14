f0ae5288d69b69cff9a8e8977a26f71531953b1d
Vineet Vijaykumar <mysticvin@gmail.com>
2025-05-14 14:45:26 -0700

    update database.json, update main

--- library/main.py
+++ library/main.py
@@ -1,56 +1,48 @@
-from shopping_cart.item import Item
-from shopping_cart.cart import Cart
+from library.book import Book
+from library.library import Library
 
 if __name__ == "__main__":
 
     #the main database for the cart to pass to the class
     database = "./database.json"
-    my_cart = Cart(database)
+    my_library = Library(database)
 
-    #search for an item
+    # search for an item
     print("Searching item...")
-    results = my_cart.search_items("apple")
+    results = my_library.search_book("relentless")
     print("------------------------")
 
-    #get the total amount of items in the cart
-    print("Total items in the current cart:")
-    total_item_count = my_cart.get_total_item_count()
-    print(f"Total items count: {total_item_count}")
+    # Create an Item object so it can be added to the cart
+    print("Adding books to the library:...")
+    print("------------------------")
+    book_one = Book("Repertoire", "Asi Wind")
+    my_library.add_book_to_library(book_one)
+    book_two = Book("Prism", "Max Maven")
+    my_library.add_book_to_library(book_two)
 
+    # get all the books in the library
     print("------------------------")
-
-    #Create an Item object so it can be added to the cart
-    print("Adding items to the cart:...")
+    print("Getting all the books in the library:")
     print("------------------------")
-    milk = Item("milk","liquid",1.23)
-    my_cart.add_item_to_cart(milk)
-    onion = Item("onion", "vegetable", 0.43)
-    my_cart.add_item_to_cart(onion)
-
-    #get all the items in the cart
-    print("------------------------")
-    print("Getting all the items in the cart:")
-    print("------------------------")
-    my_cart.get_all_items(verbose=1)
+    my_library.get_all_books(verbose=1)
 
     print("------------------------")
 
-    #remove all items by name or type
+    # remove all items by name or type
     print("Removing all instances of an item:")
     print("------------------------")
-    my_cart.remove_items_from_cart_by_query("milk")
+    my_library.remove_books_from_library_by_query("Repertoire")
+    my_library.remove_books_from_library_by_query("Prism")
+
     print("------------------------")
     print("\n")
-    #removes a selected item
+    # removes a selected item
     print("Removing a specific item by selection:")
     print("------------------------")
-    my_cart.remove_items_from_cart_by_selection()
+    my_library.remove_books_from_library_by_selection()
     print("------------------------")
 
     print("All the current items in the cart:")
-    my_cart.get_all_items(verbose=1)
+    my_library.get_all_books(verbose=1)
     print("------------------------")
 
-    print("Total price for all the items in the cart:")
-    total_price = my_cart.get_total_price_of_items()
-    print(f"${total_price}")
